b42c0b89e4c598ab7ef90c601ea93d3cabdfee04
cortex05 <havinginitiative@gmail.com>
2026-08-11 16:17:25 -0500

    feat: made calls for house-vote roll call and members

--- src/clients/voteClient.ts
+++ src/clients/voteClient.ts
@@ -1,12 +1,10 @@
 import { billClient } from "./billClient";
+import { congressClient } from "./congressClient";
 
 const getHouseVotes = async () => {
+  const bill = await billClient.getBillActions(119, "hr", 1);
 
-    const bill = await billClient.getBillActions(119, "hr", 1);
-
-    console.log(`Bill actions fetched successfully: `, JSON.stringify(bill, null, 2));
-
-    
+  console.log(`Bill actions fetched successfully: `, JSON.stringify(bill, null, 2));
   // const response = await congressClient.get<CongressHouseVoteResponse>(
   //   `/house-vote/${congress}`
   // );
@@ -14,6 +12,30 @@ const getHouseVotes = async () => {
   // return response.houseRollCallVotes;
 };
 
+const getHouseRollCall = async (
+  congress: number,
+  session: number,
+  rollCallNumber: number
+): Promise<any> => {
+  const response = await congressClient.get<any>(
+    `/house-vote/${congress}/${session}/${rollCallNumber}`
+  );
+  return response;
+}; 
+
+const getHouseRollCallMembers = async (
+  congress: number,
+  session: number,
+  rollCallNumber: number
+): Promise<any> => {
+  const response = await congressClient.get<any>(
+    `/house-vote/${congress}/${session}/${rollCallNumber}/members`
+  );
+  return response;
+}; 
+
 export const voteClient = {
   getHouseVotes,
+  getHouseRollCall,
+  getHouseRollCallMembers,
 };
--- src/scripts/importVotes.ts
+++ src/scripts/importVotes.ts
@@ -1,8 +1,11 @@
 import { voteClient } from "../clients/voteClient";
 
 export const main = async () => {
-  await voteClient.getHouseVotes();
-  // console.log(`House votes fetched successfully: `, JSON.stringify(result, null, 2));
+  // await voteClient.getHouseVotes();
+  // const result = await voteClient.getHouseRollCall(119,1,138);
+  const result = await voteClient.getHouseRollCallMembers(119,1,1);
+
+  console.log(`House votes fetched successfully: `, JSON.stringify(result, null, 2));
 };
 
 main().catch((error) => {

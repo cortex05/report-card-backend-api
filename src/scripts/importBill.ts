7c60dbad4e5460652c926fd08392833cf5c2b604
cortex05 <havinginitiative@gmail.com>
2026-09-01 16:48:15 -0500

    feat: Refactored import billService to use syncBill

--- src/scripts/importBill.ts
+++ src/scripts/importBill.ts
@@ -1,10 +1,11 @@
 // import { billClient } from "../clients/billClient";
+import { billClient } from "../clients/billClient";
 import { importBill } from "../services/billImportService";
 
 export const main = async () => {
-  const congressNumber = process.argv[2];
+  const congressNumber = parseInt(process.argv[2]);
   const billType = process.argv[3];
-  const billNumber = process.argv[4];
+  const billNumber = parseInt(process.argv[4]);
 
   if (!congressNumber) {
     console.error("Please provide a congress number as an argument.");
@@ -20,10 +21,12 @@ export const main = async () => {
   }
   console.log("Importing bill...");
 
+  const bill = await importBill(congressNumber, billType, billNumber);
+
   // const bill = await billClient.getBill(119, "hr", 1);
 
   // console.log(`Bill fetched successfully: `, JSON.stringify(bill, null, 2));
-  const bill = await importBill(Number(congressNumber), billType, Number(billNumber));
+  // const bill = await importBill(Number(congressNumber), billType, Number(billNumber));
 
   console.log(`Bill fetched successfully: `, JSON.stringify(bill, null, 2));
 };
--- src/services/billImportService.ts
+++ src/services/billImportService.ts
@@ -1,52 +1,72 @@
 import { db } from "../db";
 import { billClient } from "../clients/billClient";
-import { mapCongressBillToInsert } from "../mappers/billMapper";
+import { mapCongressBillToInsert, mapRecordedVotesFromBillActions } from "../mappers/billMapper";
 import { mapCongressSponsorToPolitician } from "../mappers/politicianMapper";
 import { mapCongressSponsorToBillInsert } from "../mappers/billSponsorMapper";
 import { billRepository } from "../repositories/billRepository";
 import { politicianRepository } from "../repositories/politicianRepository";
 import { billSponsorRepository } from "../repositories/billSponsorRepository";
+import { CongressBill } from "../types/congress/bill";
+import { Database } from "../db/types";
 
 export const importBill = async (
   congress: number,
   billType: string,
   billNumber: number
 ) => {
-  const congressBill = await billClient.getBill(congress, billType, billNumber);
-  const bill = mapCongressBillToInsert(congressBill);
+  const congressBill = await billClient.getBill(
+    congress,
+    billType,
+    billNumber
+  );
+
+  const actions = await billClient.getBillActions(congress, billType, billNumber)
+  const recordedVotes = mapRecordedVotesFromBillActions(actions);
+
+  console.log("Recorded votes: ", recordedVotes)
 
   return db.transaction(async (tx) => {
-    const existing = await billRepository.getByIdentifier(
-      tx,
-      bill.congress,
-      bill.billType,
-      bill.billNumber
-    );
-
-    const persistedBill = existing
-      ? await billRepository.update(tx, existing.id, { ...bill, lastSyncedAt: new Date() })
-      : await billRepository.create(tx, bill);
-
-    for (const sponsor of congressBill.sponsors) {
-      const existingPolitician = await politicianRepository.getByBioguideId(tx, sponsor.bioguideId);
-      const politician = existingPolitician ?? await politicianRepository.create(
-        tx,
-        mapCongressSponsorToPolitician(sponsor)
-      );
-
-      const billSponsor = mapCongressSponsorToBillInsert(persistedBill.id, politician.id, sponsor);
-      const existingSponsor = await billSponsorRepository.getByDefinition(
-        tx,
-        billSponsor.billId,
-        billSponsor.politicianId,
-        billSponsor.role
-      );
-
-      if (!existingSponsor) {
-        await billSponsorRepository.create(tx, billSponsor);
-      }
-    }
-
-    return persistedBill;
+    return syncBill(tx, congressBill);
   });
 };
+
+export const syncBill = async (
+  tx: Database,
+  congressBill: CongressBill
+) => {
+  const bill = mapCongressBillToInsert(congressBill);
+
+  const existing = await billRepository.getByIdentifier(
+    tx,
+    bill.congress,
+    bill.billType,
+    bill.billNumber
+  );
+
+  const persistedBill = existing
+    ? await billRepository.update(tx, existing.id, { ...bill, lastSyncedAt: new Date() })
+    : await billRepository.create(tx, bill);
+  
+
+  for (const sponsor of congressBill.sponsors) {
+    const existingPolitician = await politicianRepository.getByBioguideId(tx, sponsor.bioguideId);
+    const politician = existingPolitician ?? await politicianRepository.create(
+      tx,
+      mapCongressSponsorToPolitician(sponsor)
+    );
+
+    const billSponsor = mapCongressSponsorToBillInsert(persistedBill.id, politician.id, sponsor);
+    const existingSponsor = await billSponsorRepository.getByDefinition(
+      tx,
+      billSponsor.billId,
+      billSponsor.politicianId,
+      billSponsor.role
+    );
+
+    if (!existingSponsor) {
+      await billSponsorRepository.create(tx, billSponsor);
+    }
+  }
+
+  return persistedBill;
+}
